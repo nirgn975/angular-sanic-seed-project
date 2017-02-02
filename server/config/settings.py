1eb2dd516d7ec10063986b4b7ab45411d2574cee
nirgn975 <nir@galon.io>
2017-02-02 11:56:19 +0200

    Create postgres database in travis-ci

--- server/config/settings.py
+++ server/config/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'server',
-        'USER': 'nirgalon',
+        'USER': 'postgres',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': 5432,
