11cecf66e755c40f2b5b88ac34a7c8b6bbc47bb6
nirgn975 <nir@galon.io>
2017-02-02 20:11:44 +0200

    Add test for users api

--- server/api/tests.py
+++ server/api/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UsersApiTestCase(APITestCase):
+    def setUp(self):
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin12345')
+
+    def test_get_users_objects(self):
+        response = self.client.get('/api/users/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['email'], 'admin@example.com')
+        self.assertEqual(response.data[0]['username'], 'admin')
+        self.assertEqual(response.data[0]['url'], 'http://testserver/api/users/1/')
