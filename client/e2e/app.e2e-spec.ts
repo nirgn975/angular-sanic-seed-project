1256b42f197a2556bf4e6a8456b120422aeb06b2
nirgn975 <nir@galon.io>
2017-03-02 01:03:18 +0200

    Upgrade ng-cli version

--- client/e2e/app.e2e-spec.ts
+++ client/e2e/app.e2e-spec.ts
@@ -1,9 +1,14 @@
 import { ClientPage } from './app.po';
 
-describe('client App', function() {
+describe('client App', () => {
   let page: ClientPage;
 
   beforeEach(() => {
     page = new ClientPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
   });
 });
--- client/e2e/app.po.ts
+++ client/e2e/app.po.ts
@@ -4,4 +4,8 @@ export class ClientPage {
   navigateTo() {
     return browser.get('/');
   }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
 }
