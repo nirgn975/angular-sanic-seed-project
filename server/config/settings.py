19a6101a894d14cbf03964b239b703f59a49287c
Nir Galon <nir@galons.io>
2017-07-23 00:55:58 +0300

    Add permissions to API (#68)
    * Add auth to server and fix user.service to accommodate it
    
    * Fix tests
    
    * Fix test

--- server/config/settings.py
+++ server/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', True)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 CORS_ORIGIN_WHITELIST = (
     'localhost:4200',
@@ -135,6 +135,13 @@ LOGGING = {
             'propagate': True,
         },
     },
+}
+
+# REST API
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 }
 
 
