d5a9807282310bbfe3a6b7a112a96aa1be1f5703
nirgn975 <nir@galon.io>
2017-12-28 10:57:47 +0200

    Remove e2e default tests

--- client/e2e/app.e2e-spec.ts
+++ client/e2e/app.e2e-spec.ts
@@ -6,9 +6,4 @@ describe('client App', () => {
   beforeEach(() => {
     page = new AppPage();
   });
-
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
 });
--- client/e2e/app.po.ts
+++ client/e2e/app.po.ts
@@ -4,8 +4,4 @@ export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
-
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
 }
