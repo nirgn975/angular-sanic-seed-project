2d99b7dc5cd7881c0fbd057e12e640b04505ad15
Nir Galon <nir@galon.io>
2019-10-07 16:21:41 +0300

    Fix travis

--- client/src/app/modules/users/components/users-list/users-list.component.spec.ts
+++ client/src/app/modules/users/components/users-list/users-list.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { UsersListComponent } from './users-list.component';
+import { reducers } from '../../reducers';
 
 describe('UsersListComponent', () => {
   let component: UsersListComponent;
   let fixture: ComponentFixture<UsersListComponent>;
 
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UsersListComponent ]
+      declarations: [ UsersListComponent ],
+      imports: [ StoreModule.forRoot(reducers) ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     })
     .compileComponents();
   }));
--- client/src/app/modules/users/services/user.service.spec.ts
+++ client/src/app/modules/users/services/user.service.spec.ts
@@ -1,12 +1,31 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let injector: TestBed;
+  let service: UserService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+          HttpClientTestingModule,
+        ],
+        providers: [ UserService ],
+    });
+
+    injector = getTestBed();
+    service = injector.get(UserService);
+    httpMock = injector.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
 
   it('should be created', () => {
-    const service: UserService = TestBed.get(UserService);
     expect(service).toBeTruthy();
   });
 });
